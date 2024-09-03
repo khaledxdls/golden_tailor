d6b18b9dc6479f331f83cf7dcea75889f3e5b43c
Khaled Bachir Delassi <khaleddelassi03@gmail.com>
2024-09-03 18:34:11 +0100

    add navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import BussinesInfo from "./BussinesInfo";
@@ -9,8 +9,7 @@ function NavBar() {
   const [isMobile, setIsMobile] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const location = useLocation();
-  
-  
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 1200);
@@ -29,14 +28,29 @@ function NavBar() {
     }
   }, [location]);
   const toggleDropdown = () => {
-    setIsDropdownOpen(prev => !prev);
+    setIsDropdownOpen((prev) => !prev);
   };
 
   return (
-    <nav className="z-10 bg-primary-50 fixed flex items-center justify-between px-[2.4rem] py-[1.2rem] w-[90%] left-1/2 transform -translate-x-1/2 mt-5 ">
-      <Logo />
-      <MainNav />
-      <BussinesInfo />
+    <nav className="bg-primary-50 fixed w-[90%] left-1/2 transform -translate-x-1/2 mt-5 px-[2.4rem] py-[1.2rem] flex items-center justify-between">
+      <div className="flex items-center">
+        {isMobile && (
+          <button onClick={toggleDropdown} className="focus:outline-none mr-4">
+            {isDropdownOpen ? (
+              <XMarkIcon className="h-8 w-8 text-gray-600" />
+            ) : (
+              <Bars3Icon className="h-8 w-8 text-gray-600" />
+            )}
+          </button>
+        )}
+        <Logo />
+      </div>
+      {isMobile && isDropdownOpen && (
+        <div className="absolute bg-white shadow-md mt-2 w-full top-full left-0">
+          <MainNav isDropdownOpen={isDropdownOpen} />
+        </div>
+      )}
+      {!isMobile && <MainNav isDropdownOpen={false} />} <BussinesInfo />
     </nav>
   );
 }
