5ffdb0b17051c96258392ed4d9af00eefccc15fc
Khaled Bachir Delassi <khaleddelassi03@gmail.com>
2024-09-04 15:35:50 +0100

    add responsive to about section

--- src/components/AboutusHomeSection.jsx
+++ src/components/AboutusHomeSection.jsx
@@ -4,11 +4,11 @@ import ButtonPrimary from "./ButtonPrimary";
 function AboutusHomeSection() {
   const navigate = useNavigate();
   return (
-    <div className=" -mt-24 rounded-[10px] text-center px-40 py-10 bg-slate-50 mx-10 ">
-      <h2 class="text-3xl font-bold text-[#134e4a] leading-[1.2] mb-[1.2rem]">
-        Golden Tailor
+    <div className="mt-10 md:-mt-24 rounded-[10px] text-center px-5 py-10 md:px-40 bg-slate-50 mx-5 md:mx-10">
+      <h2 className="text-2xl md:text-3xl font-bold text-[#134e4a] leading-[1.2] mb-4 md:mb-[1.2rem]">
+        Golden Tailor{" "}
       </h2>{" "}
-      <p class=" mb-6">
+      <p className="mb-4 md:mb-6">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit.Dolorum ratione
         incidunt illo necessitatibus cum aliquid accusamus distinctio
         dignissimos placeat ducimus venia.Lorem ipsum dolor sit, amet
@@ -19,7 +19,6 @@ function AboutusHomeSection() {
         distinctio dignissimos placeat ducimus venia.{" "}
       </p>{" "}
       <ButtonPrimary onClick={() => navigate("/about")}>
-        {" "}
         About Us{" "}
       </ButtonPrimary>{" "}
     </div>
